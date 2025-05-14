3175f494972e9c2f73b623a9e01e2cc15705db9f
Patrick Nii <patrick.nii@aol.com>
2025-05-14 17:10:30 +0200

    maj layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Box, ChakraProvider, Container } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import StripeBackground from '@/components/ui/StripeBackground';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -10,13 +11,15 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <ChakraProvider>
-      <Box minH="100vh" bg="gray.50">
+      <StripeBackground>
+        <Box minH="100vh" position="relative" zIndex="1">
         <Header />
         <Container maxW="container.xl" py={8}>
           {children}
         </Container>
         <Footer />
       </Box>
+      </StripeBackground>
     </ChakraProvider>
   );
 };
